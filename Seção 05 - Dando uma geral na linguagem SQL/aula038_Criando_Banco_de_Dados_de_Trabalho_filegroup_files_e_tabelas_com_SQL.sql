f59bfc82b0af1a4c1db4ceefa724d14680c575b8
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-06-06 21:52:11 -0300

    SQL_Server_DBA_Sandro_Servino, 06/06/2026 (noite)

--- Seção 05 - Dando uma geral na linguagem SQL/aula038_Criando_Banco_de_Dados_de_Trabalho_filegroup_files_e_tabelas_com_SQL.sql
+++ Seção 05 - Dando uma geral na linguagem SQL/aula038_Criando_Banco_de_Dados_de_Trabalho_filegroup_files_e_tabelas_com_SQL.sql
@@ -6,86 +6,79 @@
 /* CRIAR OBJETOS */
 
 -- Selecionar o banco master: Procedimento padrão para criar um novo banco de dados com seus files e filegroups no SQL Server.
-use master
-go
+use master;
 
 
 -- Criar banco de dados:
 create database CLIENTES
-	on primary (
-		name = 'CLIENTES', 
-		filename = 'M:\Data\CLIENTES.mdf', 
-		size = 64MB, 
-		maxsize = unlimited, 
-		filegrowth = 64MB)
-	log on (
-		name = 'CLIENTE_log', 
-		filename = 'N:\Log\CLIENTES_log.ldf', 
-		size = 64MB, 
-		maxsize = unlimited, 
-		filegrowth = 64MB)
-go
+on primary(
+	name = 'CLIENTES', 
+	filename = 'M:\Data\CLIENTES.mdf', 
+	size = 64MB, 
+	maxsize = unlimited, 
+	filegrowth = 64MB
+)
+log on(
+	name = 'CLIENTE_log', 
+	filename = 'N:\Log\CLIENTES_log.ldf', 
+	size = 64MB, 
+	maxsize = unlimited, 
+	filegrowth = 64MB
+);
 
 
 -- Altera o proprietário (owner) do banco de dados: Sempre especificar o "sa".
 alter authorization
-	on database::CLIENTES
-	to sa
-go
+on database::CLIENTES
+to sa;
 
 
 -- Criar filegroup:
 alter database CLIENTES
-add filegroup CLIENTES_ANTIGOS_FG
-go
+add filegroup CLIENTES_ANTIGOS_FG;
 
 alter database CLIENTES
-add filegroup CLIENTES_FUTUROS_FG
-go
+add filegroup CLIENTES_FUTUROS_FG;
 
 
 -- Criar file:
 alter database CLIENTES
-add file (
+add file(
 	name = CLIENTES_ANTIGOS,
 	filename = 'M:\Data\CLIENTES_ANTIGOS.ndf',
 	size = 64MB,
 	maxsize = unlimited,
 	filegrowth = 64MB
 )
-to filegroup CLIENTES_ANTIGOS_FG
-go
+to filegroup CLIENTES_ANTIGOS_FG;
 
 alter database CLIENTES
-add file (
+add file(
 	name = CLIENTES_FUTUROS,
 	filename = 'M:\Data\CLIENTES_FUTUROS.ndf',
 	size = 64MB,
 	maxsize = unlimited,
 	filegrowth = 64MB
 )
-to filegroup CLIENTES_FUTUROS_FG
-go
+to filegroup CLIENTES_FUTUROS_FG;
 
 
 -- Selecionar o banco CLIENTES: Sair do banco master para criar os objetos no banco CLIENTES.
-use CLIENTES
-go
+use CLIENTES;
 
 
 -- Criar tabela: Não informando o filegroup.
-create table clientes (
+create table clientes(
 	-- O "identity" faz o SQL Server gerar números sequenciais automaticamente para este campo.
 	idCliente int identity primary key,
 	nome varchar(50),
 	cidade varchar(15),
 	uf varchar(2)
-)
-go
+);
 
 
 -- Criar tabela: Informando o filegroup.
-create table clientesFuturos (
+create table clientesFuturos(
 	-- O "identity" faz o SQL Server gerar números sequenciais automaticamente para este campo.
 	idCliente int identity primary key,
 	nome nvarchar(50),
@@ -93,56 +86,45 @@ create table clientesFuturos (
 	uf varchar(2)
 )
 -- Expecificando o filegroup da tabela.
-on CLIENTESFUTUROS_FG
-go
+on CLIENTESFUTUROS_FG;
 
-create table clientesAntigos (
+create table clientesAntigos(
 	idCliente int identity primary key,
 	nome nvarchar(50),
 	cidade nvarchar(15),
 	uf varchar(2)
 )
-on CLIENTESANTIGOS_FG
-go
+on CLIENTESANTIGOS_FG;
 
 
 /* DELETAR OBJETOS */
 
 -- Deletar tabela:
-drop table clientes
-go
+drop table clientes;
 
-drop table clientesFuturos
-go
+drop table clientesFuturos;
 
-drop table clientesAntigos
-go
+drop table clientesAntigos;
 
 
 -- Deletar file criado:
 alter database CLIENTES
-remove file CLIENTESANTIGOS
-go
+remove file CLIENTESANTIGOS;
 
 alter database CLIENTES
-remove file CLIENTESFUTUROS
-go
+remove file CLIENTESFUTUROS;
 
 
 -- Deletar filegroup criado:
 alter database CLIENTES
-remove filegroup CLIENTESANTIGOS_FG
-go
+remove filegroup CLIENTESANTIGOS_FG;
 
 alter database CLIENTES
-remove filegroup CLIENTESFUTUROS_FG
-go
+remove filegroup CLIENTESFUTUROS_FG;
 
 
 -- Deletar o banco de dados:
 -- Selecionar o banco master, procedimento padrão para deletar um banco de dados criado.
-use master
-go
+use master;
 
-drop database CLIENTES
-go
+drop database CLIENTES;
