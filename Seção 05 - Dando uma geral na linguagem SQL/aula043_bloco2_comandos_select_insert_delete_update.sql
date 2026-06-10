d7bb6d428e9173d59c890a9904a122c8c5460e27
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-06-10 16:48:28 -0300

    SQL_Server_DBA_Sandro_Servino, 10/06/2026 (tarde)

--- Seção 05 - Dando uma geral na linguagem SQL/aula043_bloco2_comandos_select_insert_delete_update.sql
+++ Seção 05 - Dando uma geral na linguagem SQL/aula043_bloco2_comandos_select_insert_delete_update.sql
@@ -1,13 +1,35 @@
-/*
-	Selecionando o banco CLIENTES
-*/
-
-use CLIENTES
-go
+-- Selecionando o banco CLIENTES
+use CLIENTES;
 
 
-/*
-	CLAUSE
-*/
+-- SQL GROUP BY:
+select country as [PAÍSES],
+	   count(id) as [CÓDIGOS]
+from customer
+-- O "where" sempre é inserida entes do "group by"
+where country is not null
+group by country;
 
--- 
+
+select country as [PAÍSES],
+	   city as [CIDADES],
+	   count(id) as [CÓDIGOS]
+from customer
+-- O "where" sempre é inserida entes do "group by"
+where country is not null
+group by country, city
+order by country, city;
+
+
+-- SQL ALIAS: Apelidos.
+select count(c.id) as [TotalCustomers],
+	   c.country as [Nation]
+from customer c
+group by c.country;
+
+
+select count(c.id) as [numberClients],
+	   c.country
+from  customer c
+group by c.country
+order by c.country;
